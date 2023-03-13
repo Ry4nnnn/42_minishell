a893a488f7be43c1565a1422d8d91d1ddf15cd1a
wangxuerui2003 <69043064+wangxuerui2003@users.noreply.github.com>
2023-03-13 15:45:01 +0000

    update notes, create a memo.txt to remind what to do

--- note.sh
+++ note.sh
@@ -1,4 +1,4 @@
-# Builtin tests
+# Builtins
 cd
 env
 pwd
@@ -7,19 +7,62 @@ unset
 exit
 echo
 
-# Basic tests
-echo "hi"
-echo hi
+# env tests
+env
 
+# echo tests
 export A=z
 echo $A
 echo "$A"
 echo '$A'
 echo "$A$A"
 echo "$DNE$A"
+echo -n hello
+
 
 export Z="ho hi"
 ec$Z
 
 echo "$A'$A'"
+echo "$A_$A"
+echo "$A1$A"
+echo $?
+
+
+# cd test
+cd
+cd ~
+cd ..
+cd -
+cd abc
+cd cd
+echo hi | cd      # cd in pipes
+
+# env test
+env
+
+# pwd test
+pwd
+pwd abc
+
+# unset test
+unset
+unset A
+echo hi | unset USER    # unset in pipes
+
+# export test
+export
+export Z
+export Z=
+export Z=abc
+export Z=$A
+export Z="$A"
+export Z='$A'
+echo hi | export A=abc    # export in pipes
+
+# exit test
+exit
+exit 1
+exit | echo hi        # exit in pipes
+
 
